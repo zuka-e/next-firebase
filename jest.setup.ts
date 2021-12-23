7f85d2696168b6d2d2b3f9f7ab7b1eca612ec81a
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-12-23 23:40:56 +0900

    Start the worker in the browser, while in tests start the mock server

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,17 @@
 // See: https://github.com/testing-library/jest-dom
 
 import '@testing-library/jest-dom/extend-expect';
+
+import { server } from '@test/mocks/server';
+
+beforeAll(() => {
+  server.listen(); // Enable the mocking.
+});
+
+afterEach(() => {
+  server.resetHandlers(); // Reset any runtime handlers tests may use.
+});
+
+afterAll(() => {
+  server.close(); // Clean up once the tests are done.
+});
